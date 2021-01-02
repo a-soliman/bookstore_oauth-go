8d36ef3a47d081503cf82fe7621b9a817f00d1f1
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-02 10:55:53 -0800

    created the oauth shared library

---
+++ oauth/oauth.go
@@ -0,0 +1,124 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/a-soliman/bookstore_oauth-go/oauth/errors"
+	"github.com/federicoleon/golang-restclient/rest"
+)
+
+const (
+	headerXPublic   = "X-Public"
+	headerXClientID = "X-Client-Id"
+	headerXCallerID = "X-Caller-Id"
+
+	paramAccessToken = "access_token"
+)
+
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://localhost:8080",
+		Timeout: 200 * time.Millisecond,
+	}
+)
+
+type accessToken struct {
+	ID       string `json:"id"`
+	UserID   int64  `json:"user_id"`
+	ClientID int64  `json:"client_id"`
+}
+
+type oauthInterface interface{}
+
+// IsPublic returns true if the request is public
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+// GetClientID returns the clientID from the request header or 0 if an error occurred
+func GetClientID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+
+	ClientID, err := strconv.ParseInt(request.Header.Get(headerXClientID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return ClientID
+}
+
+// GetCallerID returns the callerID from the request header or 0 if an error occurred
+func GetCallerID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+
+	callerID, err := strconv.ParseInt(request.Header.Get(headerXCallerID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerID
+}
+
+// AuthenticateRequest authenticates a given request, after clearing it from X headers, then appending clientId and the callerId headers
+func AuthenticateRequest(request *http.Request) *errors.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenID := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenID == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenID)
+	if err != nil {
+		return err
+	}
+
+	request.Header.Add(headerXClientID, fmt.Sprintf("%v", at.ClientID))
+	request.Header.Add(headerXCallerID, fmt.Sprintf("%v", at.UserID))
+
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+	request.Header.Del(headerXClientID)
+	request.Header.Del(headerXCallerID)
+}
+
+func getAccessToken(accessTokenID string) (*accessToken, *errors.RestErr) {
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/:access_token_id/%s", accessTokenID))
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerError("invalid restclient response while trying to get access token")
+	}
+
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
+			return nil, errors.NewInternalServerError("invalid error interface while trying to get access token")
+		}
+		return nil, &restErr
+	}
+
+	var at accessToken
+
+	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		return nil, errors.NewInternalServerError("error while trying to unmarshal users response to get access token")
+	}
+	return &at, nil
+}
