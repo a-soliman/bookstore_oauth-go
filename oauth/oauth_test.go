3b80d465d05568705f6c4f0015219863f0359742
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-08 09:02:44 -0800

    testing is public

--- oauth/oauth_test.go
+++ oauth/oauth_test.go
@@ -1,6 +1,7 @@
 package oauth
 
 import (
+	"net/http"
 	"os"
 	"testing"
 
@@ -19,4 +20,23 @@ func TestOauthConstants(t *testing.T) {
 	assert.EqualValues(t, "X-Client-Id", headerXClientID)
 	assert.EqualValues(t, "X-Caller-Id", headerXCallerID)
 	assert.EqualValues(t, "access_token", paramAccessToken)
+}
+
+func TestIsPublicNilRequest(t *testing.T) {
+	isPublic := IsPublic(nil)
+	assert.True(t, isPublic)
+}
+func TestIsPublicTrue(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/testing", nil)
+	req.Header.Add(headerXPublic, "true")
+	isPublic := IsPublic(req)
+
+	assert.True(t, isPublic)
+}
+
+func TestIsPublicFalse(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/testing", nil)
+	isPublic := IsPublic(req)
+
+	assert.False(t, isPublic)
 }
